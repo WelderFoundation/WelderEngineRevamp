5372da193df47e89a5bfe831b43ec29f8932fb53
Robert Campbell <waprave@gmail.com>
2022-12-15 10:05:19 -0500

    Bugfix: Fixed export path

--- Libraries/Gameplay/EmscriptenExportTarget.cpp
+++ Libraries/Gameplay/EmscriptenExportTarget.cpp
@@ -33,7 +33,7 @@ void EmscriptenExportTarget::ExportApplication()
 {
   {
     ProfileScopeFunction();
-    String outputDirectory = FilePath::Combine(GetTemporaryDirectory(), "Web", "ZeroExport");
+    String outputDirectory = mExporter->mOutputDirectory;
 
     Status status;
     mExporter->CopyContent(status, outputDirectory, this);
